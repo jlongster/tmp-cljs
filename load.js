f3777ff81e7a2be9466f20a020ace92faa10705a
David Nolen <dnolen@cognitect.com>
2014-08-21 11:00:31 -0400

    fix up dependencies, fix up csp.core, add load.js script

---
+++ load.js
@@ -0,0 +1,4 @@
+require("./out/goog/bootstrap/nodejs.js");
+require("./out/main.js");
+goog.require("csp.core");
+goog.require("cljs.nodejscli");
--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1896"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
-  :plugins [[lein-cljsbuild "0.2.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
                         :compiler {:output-to "out/main.js"
                                    :output-dir "out"
-                                   :optimizations :simple
+                                   :optimizations :none
                                    :target :nodejs
                                    :pretty-print true}}]}
   :main csp.core
